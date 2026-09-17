c48d707b723f768333db303542d415fdd93887be
Luis Alejandro <aluis@frba.utn.edu.ar>
2026-09-17 17:03:31 -0300

    item distributions

---
+++ Contents/mods/Wishes_DragonBalls/42/media/lua/server/dragon_ball_distributions.lua
@@ -0,0 +1,54 @@
+require 'Items/ProceduralDistributions'
+
+local procList = ProceduralDistributions["list"]
+
+local function insertToDistribution(distributionID, item, chance)
+    table.insert(procList[distributionID].items, item)
+    table.insert(procList[distributionID].items, chance)
+end
+
+local function addDragonBallToDistributions(id_dragonBall)
+    local general_spawn_chance = SandboxVars.WishesDragonBalls.Setting_ChanceToSpawnAnywhere -- 0.00001
+    for distribution, _ in pairs(procList) do
+        insertToDistribution(distribution, id_dragonBall, general_spawn_chance)
+    end
+
+    local special_spawn_chance = SandboxVars.WishesDragonBalls.Setting_ChanceToSpawnSpecial -- 1
+    insertToDistribution("CrateToys"                , id_dragonBall, special_spawn_chance)
+    insertToDistribution("ScienceMisc"              , id_dragonBall, special_spawn_chance)
+    insertToDistribution("SurvivalGear"             , id_dragonBall, special_spawn_chance)
+    insertToDistribution("CrateRandomJunk"          , id_dragonBall, special_spawn_chance * 2)
+    insertToDistribution("CrateFitnessWeights"      , id_dragonBall, special_spawn_chance)
+    insertToDistribution("Antiques"                 , id_dragonBall, special_spawn_chance * 4)
+    insertToDistribution("ArtSupplies"              , id_dragonBall, special_spawn_chance * 4)
+    insertToDistribution("CrateCamping"             , id_dragonBall, special_spawn_chance)
+    insertToDistribution("CrateCostume"             , id_dragonBall, special_spawn_chance)
+    insertToDistribution("CrateFishing"             , id_dragonBall, special_spawn_chance)
+    insertToDistribution("GiftStoreFancy"           , id_dragonBall, special_spawn_chance * 2)
+    insertToDistribution("Hobbies"                  , id_dragonBall, special_spawn_chance)
+    insertToDistribution("JunkHoard"                , id_dragonBall, special_spawn_chance * 2)
+    insertToDistribution("JewelerTools"             , id_dragonBall, special_spawn_chance)
+    insertToDistribution("JewelryGems"              , id_dragonBall, special_spawn_chance)
+    insertToDistribution("JewelryStorageAll"        , id_dragonBall, special_spawn_chance * 10)
+    insertToDistribution("LostAndFoundItems"        , id_dragonBall, special_spawn_chance)
+    insertToDistribution("PlankStashGold"           , id_dragonBall, special_spawn_chance)
+    insertToDistribution("PlankStashMisc"           , id_dragonBall, special_spawn_chance)
+    insertToDistribution("ToolStoreMisc"            , id_dragonBall, special_spawn_chance)
+    insertToDistribution("LivingRoomShelfClassy"    , id_dragonBall, special_spawn_chance)
+    insertToDistribution("OfficeDeskHomeClassy"     , id_dragonBall, special_spawn_chance)
+    insertToDistribution("WardrobeClassy"           , id_dragonBall, special_spawn_chance)
+    insertToDistribution("MayorWestPointDesk"       , id_dragonBall, special_spawn_chance * 10)
+    insertToDistribution("MayorWestPointSafe"       , id_dragonBall, special_spawn_chance * 100)
+end
+
+local function addDragonBalls()
+    addDragonBallToDistributions("DP.DragonBall_1")
+    addDragonBallToDistributions("DP.DragonBall_2")
+    addDragonBallToDistributions("DP.DragonBall_3")
+    addDragonBallToDistributions("DP.DragonBall_4")
+    addDragonBallToDistributions("DP.DragonBall_5")
+    addDragonBallToDistributions("DP.DragonBall_6")
+    addDragonBallToDistributions("DP.DragonBall_7")
+end
+
+Events.OnPreDistributionMerge.Add(addDragonBalls)
--- Contents/mods/Wishes_TheAnnoyingGenie/42/media/lua/server/GenieLamp_Distributions.lua
+++ Contents/mods/Wishes_TheAnnoyingGenie/42/media/lua/server/GenieLamp_Distributions.lua
@@ -1,6 +1,32 @@
 require 'Items/ProceduralDistributions'
 
------------------------------------------Genie Lamp-----------------------------------------
----Magazine crate--- (cambiar)
-table.insert(ProceduralDistributions["list"]["CrateMagazines"].items, "DP.GenieLamp")
-table.insert(ProceduralDistributions["list"]["CrateMagazines"].items, 0.05)
+local procList = ProceduralDistributions["list"]
+
+local function insertToDistribution(distributionID, item, chance)
+    table.insert(procList[distributionID].items, item)
+    table.insert(procList[distributionID].items, chance)
+end
+
+local function addGenieLamp()
+    local id_genieLamp = "DP.GenieLamp"
+    local general_spawn_chance = SandboxVars.WishesGenieLamp.ChanceToSpawn
+
+    insertToDistribution("CrateToys"                , id_genieLamp , general_spawn_chance)
+    insertToDistribution("ScienceMisc"              , id_genieLamp , general_spawn_chance)
+    insertToDistribution("CrateRandomJunk"          , id_genieLamp , general_spawn_chance)
+    insertToDistribution("Antiques"                 , id_genieLamp , general_spawn_chance)
+    insertToDistribution("ArtSupplies"              , id_genieLamp , general_spawn_chance)
+    insertToDistribution("GiftStoreFancy"           , id_genieLamp , general_spawn_chance)
+    insertToDistribution("JunkHoard"                , id_genieLamp , general_spawn_chance)
+    insertToDistribution("LostAndFoundItems"        , id_genieLamp , general_spawn_chance)
+    insertToDistribution("PlankStashGold"           , id_genieLamp , general_spawn_chance)
+    insertToDistribution("PlankStashMisc"           , id_genieLamp , general_spawn_chance)
+    insertToDistribution("LivingRoomShelfClassy"    , id_genieLamp , general_spawn_chance)
+    insertToDistribution("OfficeDeskHomeClassy"     , id_genieLamp , general_spawn_chance)
+    insertToDistribution("WardrobeClassy"           , id_genieLamp , general_spawn_chance)
+    insertToDistribution("MayorWestPointDesk"       , id_genieLamp , general_spawn_chance * 10)
+    insertToDistribution("MayorWestPointSafe"       , id_genieLamp , general_spawn_chance * 100)
+end
+
+
+Events.OnPreDistributionMerge.Add(addGenieLamp)
